a09d0de9d3504ed05f2277abec5dc78c8fdea495
Andrew Kazyrevich <andreister@gmail.com>
2012-05-04 15:35:54 +0200

    Implemented string similarity algo

---
+++ NAlgo.Tests/Text/SimilarityCheckerTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using NAlgo.Text;
+using NUnit.Framework;
+
+namespace NAlgo.Tests.Text
+{
+	[TestFixture]
+	public class SimilarityCheckerTests
+	{
+		[Test, TestCaseSource("Strings")]
+		public void ComputeDistance(string from, string to, decimal expectedDistance)
+		{
+			var checker = new SimilarityChecker();
+			var distance = checker.Distance(from, to);
+
+			distance = decimal.Round(distance, 2);
+			Assert.That(distance, Is.EqualTo(expectedDistance));
+		}
+
+		public IEnumerable<TestCaseData> Strings
+		{
+			get
+			{
+				return new List<TestCaseData> {
+					new TestCaseData("this is a text", "this is a text", 1m).SetName("same strings"),
+					new TestCaseData("this is a text", "thIS Is a tEXT", 1m).SetName("same strings, difference case"),
+					new TestCaseData("france", "french", 0.4m).SetName("france, french"),
+					new TestCaseData("gggg", "gg", 0.5m).SetName("gggg, gg"),
+					new TestCaseData("healed", "sealed", 0.8m).SetName("healed, sealed"),
+					new TestCaseData("healed", "healthy", 0.55m).SetName("healed, healthy"),
+					new TestCaseData("healed", "help", 0.25m).SetName("healed, help"),
+					new TestCaseData("healed", "sold", 0m).SetName("healed, sold"),
+				};
+			}
+		}
+	}
+}
---
+++ NAlgo/Text/SimilarityChecker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NAlgo.Text
+{
+	/// <summary>
+	/// A ranking string comparison algorithm. Calculates a "distance"
+	/// between the two strings via character pair similarity.
+	/// 
+	/// Inspired by Simon White http://www.catalysoft.com/articles/StrikeAMatch.html
+	/// </summary>
+	public class SimilarityChecker
+	{
+		/// <summary>
+		/// Calculates the distance between the two strings by checking
+		/// how many adjacent character pairs are contained in both strings.
+		/// Returns a percentage, where 1 means the strings are identical.
+		/// </summary>
+		/// <param name="from">First string.</param>
+		/// <param name="to">Second string.</param>
+		public decimal Distance(string from, string to)
+		{
+			var fromChunks = GetLetterChunks(from);
+			var toChunks = GetLetterChunks(to);
+			var totalChunks = fromChunks.Count + toChunks.Count;
+
+			int commonChunks = fromChunks.Sum(x => ComputeCommonChunks(x, toChunks));
+			return (2m * commonChunks) / totalChunks;
+		}
+
+		private List<string> GetLetterChunks(string text)
+		{
+			var words = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			return words.SelectMany(GetLetterChunksInWord).ToList();
+		}
+
+		private IEnumerable<string> GetLetterChunksInWord(string word)
+		{
+			if (word.Length <= 2) {
+				yield return word;
+				yield break;
+			}
+			for (int i = 0; i < word.Length - 1; i++) {
+				yield return word.Substring(i, 2);
+			}
+		}
+
+		private int ComputeCommonChunks(string chunk, List<string> chunkList)
+		{
+			int result = 0;
+			for (int i = 0; i < chunkList.Count; i++) {
+				if (chunk.Equals(chunkList[i], StringComparison.InvariantCultureIgnoreCase)) {
+					//whenever a match is found, the chunk is removed from the list,
+					//to prevent us from matching against the same character chunk multiple
+					//times (otherwise, 'GGGGG' would score a perfect match against 'GG').
+					chunkList.RemoveAt(i);
+
+					result++;
+					break;
+				}
+			}
+			return result;
+		}
+	}
+}
