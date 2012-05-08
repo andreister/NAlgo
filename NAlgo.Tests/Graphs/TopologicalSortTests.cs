7a303cbab0fe749aeb96f8ea59f4fb4f750eaa5f
Andrew Kazyrevich <andreister@gmail.com>
2012-05-08 18:10:41 +0200

    Added more tests for Topological Sort

--- NAlgo.Tests/Graphs/TopologicalSortTests.cs
+++ NAlgo.Tests/Graphs/TopologicalSortTests.cs
@@ -18,12 +18,17 @@ namespace NAlgo.Tests.Graphs
 			var algo = new TopologicalSort<string>(graph);
 			algo.Run();
 
+			foreach (var node in graph.Values) {
+				Console.WriteLine(node.Id + " " + node.Value);
+			}
+			
+
 			foreach (var nodePosition in expectedPositions.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries)) {
 				var pair = nodePosition.Split(new[] {'-'}, StringSplitOptions.RemoveEmptyEntries);
 				var node = graph[pair[0].Trim()];
 				int expectedPosition = int.Parse(pair[1]);
 
-				Assert.That(node.Value, Is.EqualTo(expectedPosition), "Node " + node.Id + " has unexpected topological order.");
+				Assert.That(node.Value, Is.EqualTo(expectedPosition), "Node '" + node.Id + "' has unexpected topological order.");
 			}
 		}
 
@@ -39,7 +44,15 @@ namespace NAlgo.Tests.Graphs
 					new TestCaseData(
 						"a b, b c, b d, c e, c f, d e, d f, e f, f i",
 						"a-1, b-2, c-4, d-3, e-5, f-6, i-7"
-					).SetName("7 nodes graph")
+					).SetName("7 nodes graph"),
+					new TestCaseData(
+						"idea development, idea testplan, development testing, testplan testing, testing release",
+						"idea-1, development-3, testplan-2, testing-4, release-5"
+					).SetName("idea-...-release"),
+					new TestCaseData(
+						"f fa, f fb, fa fa1, fa fb, fb fb1, fb fb2, fb1 fb2, fb2 fa1, fb2 fb21, fa1 fa11, fb21 fa11, fa11 fz",
+						"f-1, fa-2, fb-3, fb1-4, fb2-5, fb21-6, fa1-7, fa11-8,fz-9"
+					).SetName("convoluted PERT chart")
 				};
 			}
 		}
